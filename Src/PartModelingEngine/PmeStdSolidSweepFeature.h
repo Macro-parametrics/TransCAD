8c5e9aaa1040454359749ede3498556837cff80e
Mutahar Safdar <38282349+Mutahar-Safdar@users.noreply.github.com>
2019-08-05 21:15:23 +0900

    Update PmeStdSolidSweepFeature.h

--- Src/PartModelingEngine/PmeStdSolidSweepFeature.h
+++ Src/PartModelingEngine/PmeStdSolidSweepFeature.h
@@ -23,9 +23,17 @@ public:
 protected:
 	BODY * SweepSolid(BODY *& pProfile, BODY *& pGuideCurve);
 
+	BODY * NameNewVertices_ADD_BOL(BODY *& pBody, bool isCut = false); //For Name Matching
+	BODY * NameNewVertices_SUB_BOL(BODY *& pBody, bool isCut = true); //For Name Matching
+
+	//[Topology-Based Names]
 	void AttachName(BODY *& pBody);
-	void AttachVertexName_pointbased(BODY *& pBody);
-	void AttachFaceName_pointbased(BODY *& pBody);
+
+	//[Point-Based Names]
+	void AttachVertexName_pointbased(BODY *& pBody); //initial 
+	void AttachFaceName_pointbased(BODY *& pBody); //initial 
+	void Attach_NewVertexNames_pointbased(BODY *& pBody); //Name Matching
+	void AttachRemainingVertexNames_pointbased(BODY *& pBody); //Final Check
 
 private:
 	PmeReference * m_pProfileSketch;
